4c91f012d1f387511b72be9d6948fbe66c20ded2
Anju Anilkumar <anjuanilkumar732@gmail.com>
2026-09-10 13:32:05 +0530

    Prepare Django settings for deployment

--- cyberbullyingdetection/settings.py
+++ cyberbullyingdetection/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@74u9fy49-d_qt8-a$+@5cb266g9dlx+fyr3%ut)@%mq(v%rmz'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ["*"]
 
@@ -119,6 +119,8 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'temporary-local-secret-key')
